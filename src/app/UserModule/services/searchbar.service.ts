365c56ec299c53fe9dd08a943a21acd3f45f99eb
pspk12 <90503697+pspk12@users.noreply.github.com>
2021-10-28 13:31:52 +0530

    Update searchbar.service.ts

--- src/app/UserModule/services/searchbar.service.ts
+++ src/app/UserModule/services/searchbar.service.ts
@@ -14,12 +14,12 @@ export class SearchbarService {
   constructor(private http: HttpClient) { }
   getAllFunctionHalls():Observable<any>
   {
-    return this.http.get('http://pub or private ip:8057/api/functionhalls/');
+    return this.http.get('http://vedikaload-773761831.us-west-1.elb.amazonaws.com:8057/api/functionhalls/');
   }
   
   findFunctionhallByNameAndCity(city):Observable<any>
   {
-    return this.http.get('http://pub or private ip:8057/api/functionhallsBy/?city='+city);
+    return this.http.get('http://vedikaload-773761831.us-west-1.elb.amazonaws.com:8057/api/functionhallsBy/?city='+city);
   }
  
 }
